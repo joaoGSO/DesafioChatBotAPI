6f96dd32993564ba40dacc769ee869e51c39a989
João Gilberto Soares Oliveira <joaogilberto03@hotmail.com>
2022-08-01 23:35:17 -0300

    Created constants.

---
+++ DesafioChatBotAPI/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace DesafioChatBotAPI
+{
+    public class Constants
+    {
+        public static readonly string url = "https://api.github.com/";
+        public static readonly string repositoriesAddress = "users/takenet/repos?per_page=100&language=c%23";
+        public static readonly string language = "C#";
+        public static readonly string returnMediaType = "application/json";
+        public static readonly string productName = "DesafioChatBotAPI";
+        public static readonly string productVer = "1";
+        public static readonly int repositoriesQuantity = 5;
+    }
+}
