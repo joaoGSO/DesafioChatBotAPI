969659ac18e0f0c9daf635a027f1c446c8bedded
João Gilberto Soares Oliveira <joaogilberto03@hotmail.com>
2022-08-02 00:18:20 -0300

    Created startup.

---
+++ DesafioChatBotAPI/Startup.cs
@@ -0,0 +1,36 @@
+﻿using DesafioChatBotAPI.Services;
+
+namespace DesafioChatBotAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configurations)
+        {
+            Configuration = configurations;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<RepositoriesServices>();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder application, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                application.UseDeveloperExceptionPage();
+            }
+
+            application.UseHttpsRedirection();
+            application.UseRouting();
+            application.UseAuthorization();
+            application.UseEndpoints(endpoints => 
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
