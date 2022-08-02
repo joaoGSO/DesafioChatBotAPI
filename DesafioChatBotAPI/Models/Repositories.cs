c98fe08ef99d2d00534c1efd73db543f0da2739f
João Gilberto Soares Oliveira <joaogilberto03@hotmail.com>
2022-08-01 23:16:38 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioChatBotAPI/Models/Repositories.cs
@@ -0,0 +1,18 @@
+﻿namespace DesafioChatBotAPI.Models
+{
+    public class Repositories
+    {
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Language { get; set; }
+        public String Creation_Date { get; set; }
+
+        public Repositories(string name, string description, string language, string creation_date)
+        {
+            Name = name;
+            Description = description;
+            Language = language;
+            Creation_Date = creation_date;
+        }
+    }
+}
