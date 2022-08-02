7302f24421e61d604865d7c02c464fe7efd8f62d
João Gilberto Soares Oliveira <joaogilberto03@hotmail.com>
2022-08-02 00:12:24 -0300

    Implemented controllers.

--- DesafioChatBotAPI/Controllers/RepositoriesControllers.cs
+++ DesafioChatBotAPI/Controllers/RepositoriesControllers.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DesafioChatBotAPI.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DesafioChatBotAPI.Controllers
 {
-    public class RepositoriesController : ControllerBase
+    public class RepositoriesControllers : ControllerBase
     {
+        private readonly RepositoriesServices _repositoriesServices;
 
+        public RepositoriesControllers(RepositoriesServices repositoriesService)
+        {
+            _repositoriesServices = repositoriesService;
+        }
+
+        [HttpGet]
+        [Route("api/getrepositories")]
+        public string GetRepositories()
+        {
+            try
+            {
+                return _repositoriesServices.RepositoriesFilteredByLanguage(Constants.language, Constants.repositoriesQuantity);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
